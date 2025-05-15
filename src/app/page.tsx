86e2cd35086fe342e280209a8c69732e48ade60e
mosaictm <mosaicgroupsyr@gmail.com>
2025-05-15 20:25:55 +0300

    removed footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
 import HeroSection from '../components/sections/HeroSection';
 import ServicesSection from '../components/sections/ServicesSection';
 import PaymentMethodsSection from '../components/sections/PaymentMethodsSection';
